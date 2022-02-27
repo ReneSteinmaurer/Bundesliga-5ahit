5b88c1f52c1cb25455ed963604cf4e7292d258a8
refr <franz.reitinger@htl-wels.at>
2022-02-27 13:35:03 +0100

    initial

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>(c)HTL Wels / 5aHIT</h1>
+    <span> Projekt für den Montag Vormittag </span>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img
+      alt="Vue logo"
+      src="https://www.bundesliga.com/assets/logo/bundesliga_pos.svg"
+    />
+    <BL_Home msg="Die deutsche Bundesliga - alle Fakten" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import BL_Home from '@/components/BL_Home.vue';
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld,
+    BL_Home,
   },
 };
 </script>
