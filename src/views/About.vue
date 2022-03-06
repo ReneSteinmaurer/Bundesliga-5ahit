acd3569949df7a770f5121e3a5f421e459b154c4
refr <franz.reitinger@htl-wels.at>
2022-03-06 21:49:08 +0100

    Basisc

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,51 +11,56 @@
     <h3>Beteiligte Teams/Personen</h3>
 
     <div class="container my-4">
-      <!-- https://getbootstrap.com/docs/5.0/components/list-group/ -->
-      <ol class="list-group list-group-numbered">
-        <li
-          class="
-            list-group-item
-            d-flex
-            justify-content-between
-            align-items-start
-          "
-        >
-          <div class="ms-2 me-auto">
-            <div class="fw-bold">Projektleitung</div>
-            Helml/Reitinger
-          </div>
-          <span class="badge bg-primary rounded-pill">2</span>
-        </li>
-        <li
-          class="
-            list-group-item
-            d-flex
-            justify-content-between
-            align-items-start
-          "
-        >
-          <div class="ms-2 me-auto">
-            <div class="fw-bold">Team1</div>
-            X Y
-          </div>
-          <span class="badge bg-primary rounded-pill">2</span>
-        </li>
-        <li
-          class="
-            list-group-item
-            d-flex
-            justify-content-between
-            align-items-start
-          "
-        >
-          <div class="ms-2 me-auto">
-            <div class="fw-bold">Team 3</div>
-            Z U V
-          </div>
-          <span class="badge bg-primary rounded-pill">3</span>
-        </li>
-      </ol>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-6">
+          <!-- https://getbootstrap.com/docs/5.0/components/list-group/ -->
+          <ol class="list-group list-group-numbered">
+            <li
+              class="
+                list-group-item
+                d-flex
+                justify-content-between
+                align-items-start
+              "
+            >
+              <div class="ms-2 me-auto">
+                <div class="fw-bold">Projektleitung</div>
+                Helml/Reitinger
+              </div>
+              <span class="badge bg-primary rounded-pill">2</span>
+            </li>
+            <li
+              class="
+                list-group-item
+                d-flex
+                justify-content-between
+                align-items-start
+              "
+            >
+              <div class="ms-2 me-auto">
+                <div class="fw-bold">Team1</div>
+                X Y
+              </div>
+              <span class="badge bg-primary rounded-pill">2</span>
+            </li>
+            <li
+              class="
+                list-group-item
+                d-flex
+                justify-content-between
+                align-items-start
+              "
+            >
+              <div class="ms-2 me-auto">
+                <div class="fw-bold">Team 3</div>
+                Z U V
+              </div>
+              <span class="badge bg-primary rounded-pill">3</span>
+            </li>
+          </ol>
+        </div>
+      </div>
     </div>
   </div>
 </template>
